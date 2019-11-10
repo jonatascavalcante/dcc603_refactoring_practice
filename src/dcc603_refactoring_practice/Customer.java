46051a23b8cfd89e83375d7806b56a9aa6d983ab
jonatascavalcante <jonatascavalcante777@gmail.com>
2019-11-10 12:24:59 -0300

    Refactorig 1: Extract Method

--- src/dcc603_refactoring_practice/Customer.java
+++ src/dcc603_refactoring_practice/Customer.java
@@ -30,22 +30,7 @@ public class Customer {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
 			
-			//determine amounts for each line
-			switch(each.getMovie().getPriceCode()) {
-				case Movie.REGULAR:
-					thisAmount += 2;
-					if (each.getDaysRented() > 2)
-						thisAmount += (each.getDaysRented() - 2) * 1.5;
-					break;
-				case Movie.NEW_RELEASE:
-					thisAmount += each.getDaysRented() * 3;
-					break;
-				case Movie.CHILDRENS:
-					thisAmount += 1.5;
-					if (each.getDaysRented() > 3)
-						thisAmount += (each.getDaysRented() - 3) * 1.5;
-					break;
-			}
+			thisAmount = amountFor(each);
 			
 			//add frequent renter points
 			frequentRenterPoints++;
@@ -67,6 +52,29 @@ public class Customer {
 				" frequent renter points";
 		
 		return result;
+	}
+	
+	public double amountFor(Rental each) {
+		double thisAmount = 0;
+		
+		//determine amounts for each line
+		switch(each.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (each.getDaysRented() > 2)
+					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += each.getDaysRented() * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (each.getDaysRented() > 3)
+					thisAmount += (each.getDaysRented() - 3) * 1.5;
+				break;
+		}
+		
+		return thisAmount;
 	}
 
 }
