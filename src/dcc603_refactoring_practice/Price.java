9ee269dfd33a2e9a6659f5c552b4d1a7103eff31
jonatascavalcante <jonatascavalcante777@gmail.com>
2019-11-10 15:42:34 -0300

    Refactoring 7: Replace Conditional with Polymorphism

---
+++ src/dcc603_refactoring_practice/Price.java
@@ -0,0 +1,11 @@
+package dcc603_refactoring_practice;
+
+public abstract class Price {
+	abstract int getPriceCode();
+	
+	abstract double getCharge(int daysRented);
+	
+	int getFrequentRenterPoints(int daysRented) {
+		return 1;
+	}
+}
